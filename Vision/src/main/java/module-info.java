fb166426165d23c0d890cf47780a1893ecb53aaf
harsh4056 <hs4056@gmail.com>
2021-11-13 01:42:25 +0530

    Prod:
    Dev: Running and working

--- Vision/src/main/java/module-info.java
+++ Vision/src/main/java/module-info.java
@@ -1,11 +1,14 @@
-module com.duo.vision {
+module com.vision.vision {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.bootstrapfx.core;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
 
-    opens com.duo.vision to javafx.fxml;
-    exports com.duo.vision;
+            requires org.kordamp.bootstrapfx.core;
+
+    
+    opens com.vision.vision to javafx.fxml;
+    exports com.vision.vision;
 }
